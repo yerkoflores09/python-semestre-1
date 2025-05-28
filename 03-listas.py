ef526581be7d1f3585da4adb3365e774c5e3ed25
yerko flores <yerkonicolas.flores@alumnos.ulagos.cl>
2025-05-28 15:05:56 -0400

    agregando 03-listas

--- 03-listas.py
+++ 03-listas.py
@@ -26,3 +26,32 @@ print(estudiantes.index('hector'))
 #creando SETS
 colores = {'azul', 'rojo', 'azul', 'verde', 34}
 print(colores)
+
+#funcion APPEND
+ramos.append('introduccion a la matemática')
+print(ramos)
+
+#modificar elementos de la lista
+ramos[1] = 'comunicacion' #estoy pasando la posicion del elemento a modificar
+print(ramos)
+
+#otra forma de insertar un elemento a la lista (INSERT)
+ramos.insert(7, 'algebra')
+print(ramos)
+
+#eliminar el ultimo elemento de la lista
+ramos.pop()
+print(ramos)
+
+#ordenando los elementos de una lista de forma descendente a ascendente
+ramos.sort()
+print(ramos)
+
+#ordenando elementos de una lista segun su cantidad de carácteres
+ramos.sort(key=len) #key es una propiedad del metodo SORT y se pasa un valor que es el metodo LEN
+print(ramos)
+
+#ocupando el metodo extend (extendiendo una lista a partir de otra)
+ramitos = ['cálculo', 'autómatas']
+ramos.extend(ramitos)
+print(ramos)
