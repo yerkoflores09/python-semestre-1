39cb2dc307efe60da17be5aa920c35ceab23fb55
yerko flores <yerkonicolas.flores@alumnos.ulagos.cl>
2025-06-02 13:08:50 -0400

    modificandoa

--- ejercicios-ulagosvirtual/ejercicio-3.py
+++ ejercicios-ulagosvirtual/ejercicio-3.py
@@ -4,7 +4,7 @@ frase = str(input('ingresa una frase de maximo 30 caracteres: ')[:30])
 print(frase.upper())
 print(frase.lower())
 
-print(frase.count('a'))
-print(frase.count('A'))
+cant_a = print(frase.count('a'))
+cant_A = print(frase.count('A'))
 
-print()
+#falta el punto 4xd
